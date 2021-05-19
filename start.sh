467c9a59dcdbd909581cc3b8bfc7b15d8ffeca0c
Owen Mak <owdevel@gmail.com>
2021-05-20 01:18:52 +1000

    Added log and session existance checks to startup

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+SESSION=./aria2.session
+LOG=./aria2.log
+
+if [ ! -f "$SESSION" ]; then
+	touch "$SESSION"
+fi
+
+if [ ! -f "$LOG" ]; then
+	touch "$LOG"
+fi
+
 xdg-open ./site/index.html
 
 aria2c --conf-path=./aria.conf
